ae5a8ae95645a9bd715ff34662a18175b6927b9c
NCsuma <nallansuma@gmail.com>
2021-02-07 01:40:22 +0530

    New Flask project on Flowers

---
+++ FlowerItem.py
@@ -0,0 +1,9 @@
+class FlowerItem:
+    id = None
+    name = None
+    image_name = None
+
+    def __init__(self,id,name,image_name):
+        self.id = id
+        self.name = name
+        self.image_name = image_name
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template,url_for
+from FlowerItem import FlowerItem
 
-app = Flask(__name__,static_folder='abc')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    messages = ["Hi,welcome","you have a price","your updates are"]
-    #return "Hello, world!"
-    return render_template('index.html',messages = messages)
+    return render_template('index.html')
+
+@app.route('/flowers')
+def show_flower_images():
+    item1 = FlowerItem(1,"Cosmos","images/flower1.jpg")
+    item2 = FlowerItem(2,"Gardenblue flower", "images/flower2.jpg")
+    item3 = FlowerItem(3,"Red Anemone", "images/flower3.jpg")
+    item4 = FlowerItem(4,"Marigold", "images/flower4.jpg")
+    item5 = FlowerItem(5,"Sun Flower", "images/flower5.jpg")
+    item6 = FlowerItem(6,"Orchid", "images/flower7.jpg")
+
+    flowersgroup1 = [item1,item2,item3]
+    flowersgroup2 = [item4,item5,item6]
+    return render_template('flowers.html',flowersgroup1 = flowersgroup1 ,flowersgroup2 = flowersgroup2)
 
 @app.route('/contact')
 def contact():
